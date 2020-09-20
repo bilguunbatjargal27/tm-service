f73517d0ec3f27f5b24295b08e04b02edfaba01c
Bilguun Batjargal <bilguunbatjargal27@gmail.com>
2020-09-20 14:51:57 -0500

    fist commit

---
+++ src/main/java/cs/mum/edu/orangeteam/compro/ComproApplication.java
@@ -0,0 +1,13 @@
+package cs.mum.edu.orangeteam.compro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cs/mum/edu/orangeteam/compro/ComproApplicationTests.java
@@ -0,0 +1,13 @@
+package cs.mum.edu.orangeteam.compro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
